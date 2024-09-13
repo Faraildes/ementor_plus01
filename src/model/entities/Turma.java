cb478e3bdfcf3f2b99feb49d8197907667ad28f0
Faraildes <faraildesbarbosaferreira@gmail.com>
2024-09-13 10:48:22 -0300

    Adding database access

--- src/model/entities/Turma.java
+++ src/model/entities/Turma.java
@@ -1,20 +1,23 @@
 package model.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public class Teacher implements Serializable {
+public class Turma implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 	private String name;
+	private Double notas;
 	
-	public Teacher() {
+	public Turma() {
 	}
 
-	public Teacher(Integer id, String name) {
+	public Turma(Integer id, String name, Double notas) {
 		this.id = id;
 		this.name = name;
+		this.notas = notas;
 	}
 
 	public Integer getId() {
@@ -33,12 +36,13 @@ public class Teacher implements Serializable {
 		this.name = name;
 	}
 
+	public Double getNotas() {
+		return notas;
+	}	
+	
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -49,17 +53,18 @@ public class Teacher implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Teacher other = (Teacher) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		Turma other = (Turma) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
+		return "Turma [id=" + id + ", name=" + name + ", notas=" + notas + "]";
 	}
+	
+
 }
--- src/model/services/TeacherService.java
+++ src/model/services/TeacherService.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.TeacherDao;
 import model.entities.Teacher;
 
 public class TeacherService {
 	
+	private TeacherDao dao = DaoFactory.createTeacherDao();
+	
 	public List<Teacher> findAll(){
-		List<Teacher> list = new ArrayList<>();
-		list.add(new Teacher(1, "Joao", "12345678901", "987654321", new Date(), 5000.00, "não", "não"));
-		list.add(new Teacher(2, "Joa", "12345678901", "987654231", new Date(), 6000.00, "sim", "não"));
-		list.add(new Teacher(3, "Joana", "12345678901", "987654231", new Date(), 7000.00, "sim", "sim"));
-		return list;
+		return dao.findAll();
 	}	
 }
